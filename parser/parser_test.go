b209b30a4834fbf64c81d507d6e9cee201604560
xasterKies <samueledward40@gmail.com>
2023-12-03 23:30:46 +0100

    chore: check parsers error in tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -17,6 +17,8 @@ let foobar = 838383;
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -39,6 +41,22 @@ let foobar = 838383;
 			return
 		}
 	}
+
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 { 
+		return
+	 }
+
+	 t.Errorf("parser has %d errors", len(errors))
+	 for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	 }
+
+	 t.FailNow()
 
 }
 
