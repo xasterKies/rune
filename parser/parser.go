e661cda417df449bf30b49d1dcce86caa92bbca1
xasterKies <samueledward40@gmail.com>
2024-02-05 16:11:43 +0100

    chore(parser): added grouped expression

--- parser/parser.go
+++ parser/parser.go
@@ -49,6 +49,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 
 	p.infinixParseFns = make(map[token.TokenType]infinixParseFn)
 	p.registerInfinix(token.PLUS, p.parseInfinixExpression)
@@ -291,4 +292,16 @@ func (p *Parser) parseInfinixExpression(left ast.Expression) ast.Expression {
 
 func (p *Parser) parseBoolean() ast.Expression {
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
 }
