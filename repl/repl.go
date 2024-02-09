a6fb140a8e3d0e9bf50e126865b6431a7227e306
xasterKies <samueledward40@gmail.com>
2024-02-09 01:04:46 +0100

    feat(repl): adding rune ASCI art in repl

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,22 @@ import (
 	"io"
 
 	"github.com/xasterKies/rune/lexer"
-	"github.com/xasterKies/rune/token"
+	"github.com/xasterKies/rune/parser"
 )
 
 const PROMPT = ">> "
+
+const RUNE_LANG = `
+
+	RRRRRRR  
+	RR     RR 
+	RR     RR 
+	RRRRRRR  
+	RR   RR  
+	RR    RR 
+	RR     RR
+	
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -22,11 +34,26 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, RUNE_LANG)
+	io.WriteString(out, "Woops! We ran into some runic business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t" + msg + "\n")
 	}
 }
